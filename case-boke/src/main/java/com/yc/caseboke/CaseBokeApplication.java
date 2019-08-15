89b163cf1efae0f1eeabc8ecdfe23035109045e5
Administrator <Administrator@WIN10-811021921>
2019-08-15 09:58:36 +0800

    blog管理初始化提交

---
+++ case-boke/src/main/java/com/yc/caseboke/CaseBokeApplication.java
@@ -0,0 +1,31 @@
+package com.yc.caseboke;
+
+import java.util.Date;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@Controller
+@MapperScan("com.yc")
+public class CaseBokeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CaseBokeApplication.class, args);
+	}
+
+	@GetMapping("hello")
+	public String hello(){
+		return "hello";
+	}
+	
+	@Bean
+	public Date getNow(){
+		return new Date();
+	}
+}
---
+++ case-boke/src/test/java/com/yc/caseboke/CaseBokeApplicationTests.java
@@ -0,0 +1,16 @@
+package com.yc.caseboke;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CaseBokeApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
